1d82e9dc1764bb8d4b492017609b9805772e90e0
SHamil Takaev <shamilchechya@gmail.com>
2024-02-20 10:36:18 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, redirect
+from sqlalchemy.orm import aliased
+from data.db_session import global_init, create_session
+from data.users import User
+from data.jobs import Jobs
+from forms.user import RegisterForm
+from pprint import pprint
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    db_sess = create_session()
+    data = db_sess.query(Jobs, User).filter(User.id == Jobs.team_leader)
+    for job, user in data:
+        print((job.end_date - job.start_date).seconds // 3600)
+    return render_template("index.html", data=data, title="Работы на корабле")
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+def main():
+    global_init("db/blogs.db")
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
